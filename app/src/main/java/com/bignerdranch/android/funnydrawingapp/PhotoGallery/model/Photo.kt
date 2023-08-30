7db4a30db84bb90616de7f481acf246094d48c0c
Lan <lan.anh.7988@gmail.com>
2023-08-29 22:44:52 -0700

    Show the data by update the key to webformatURL

--- app/src/main/java/com/bignerdranch/android/funnydrawingapp/PhotoGallery/model/Photo.kt
+++ app/src/main/java/com/bignerdranch/android/funnydrawingapp/PhotoGallery/model/Photo.kt
@@ -2,8 +2,8 @@ package com.bignerdranch.android.funnydrawingapp.PhotoGallery.model
 
 import com.google.gson.annotations.SerializedName
 // map photos object to Json data
-//pageURL is the key for data from the json file
+//webformatURL is the key for data from the json file
 class Photo {
-        @SerializedName("pageURL")
+        @SerializedName("webformatURL")
         lateinit var photoLink: String
 }
